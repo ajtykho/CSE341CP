29062826e99d2298f0eee4b0ec783a6367ce02d6
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2021-01-21 16:23:01 -0700

    finished ta03

--- routes/team/ta03.js
+++ routes/team/ta03.js
@@ -1,13 +1,37 @@
-//TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const fetch = require("node-fetch");
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+
+router.get('/',(req,res,next) => {
+    fetch('https://byui-cse.github.io/cse341-course/lesson03/items.json')
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log("consoled info:", jsObject);
+        res.render('pages/ta03', {
+            items: jsObject,
+            title: 'Store Products',
+            path: '/ta03'
+        });
+    });
+});
+
+router.post('/searchedProducts',(req,res,next) => {
+    fetch('https://byui-cse.github.io/cse341-course/lesson03/items.json')
+    .then((response) => response.json())
+    .then((jsObject) => {
+        var empty = [];
+        jsObject.forEach(object => {
+            if(object.name.toLowerCase().includes(req.body.search.toLowerCase()) ) {
+                empty.push(object);
+            }
+        });
+        res.render('pages/ta03', {
+            
+            items: empty,
+            title: 'Store Products',
+            path: '/ta03'
+        });
     });
 });
 
